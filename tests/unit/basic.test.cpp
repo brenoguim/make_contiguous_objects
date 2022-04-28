35d198ad764a239dd83e796c060c0b38a5814c5e
Breno Rodrigues Guimaraes <brenorg@gmail.com>
2022-04-27 22:39:08 -0300

    Add a bunch of tests

--- tests/unit/basic.test.cpp
+++ tests/unit/basic.test.cpp
@@ -4,24 +4,78 @@
 #include <iostream>
 #include <cstring>
 
+template<class T>
+void default_construct(T* begin, T* end)
+{
+    for (auto it = begin; it != end; ++it) new (it) T();
+}
+
+// UBSAN will catch any misaligned write
+template<class T>
+void writeToRange(xtd::Rng<T>& rng)
+{
+    for (auto b = rng.begin(); b != rng.end(); ++b)
+        *b = T();
+}
+
+template<class Tup>
+struct Guard
+{
+    Guard(Tup t) : m_t(t) {}
+
+    ~Guard()
+    {
+        std::apply([] (auto&... rngs) {
+            (default_construct(rngs.begin(), rngs.end()),...);
+        }, m_t);
+
+        std::apply([] (auto&... rngs) {
+            (writeToRange(rngs),...);
+        }, m_t);
+
+        std::apply([] (auto&... rngs) {
+            (std::destroy(rngs.begin(), rngs.end()),...);
+        }, m_t);
+
+        ::operator delete((void*)std::get<0>(m_t).begin());
+    }
+
+    Tup m_t;
+};
+
 TEST_CASE( "Basic", "[make_contiguous_layout]" )
 {
-    auto l = xtd::make_contiguous_layout<int, long, char>(2, 1, 8);
-
-    std::cout << "int* : " << std::get<0>(l).begin() << " to " << std::get<0>(l).end() << std::endl;
-    std::cout << "long*: " << std::get<1>(l).begin() << " to " << std::get<1>(l).end() << std::endl;
-    std::cout << "char*: " << (void*)std::get<2>(l).begin() << " to " << (void*)std::get<2>(l).end() << std::endl;
-
-    ::operator delete((void*)std::get<0>(l).begin());
+    auto g = Guard{xtd::make_contiguous_layout<int, long, char>(2, 1, 8)};
 }
 
 TEST_CASE( "Basic - holes", "[make_contiguous_layout]" )
 {
-    auto l = xtd::make_contiguous_layout<char, int, long>(1, 2, 1);
+    auto g = Guard{xtd::make_contiguous_layout<char, int, long>(1, 2, 1)};
+}
 
-    std::cout << "char* : " << (void*)std::get<0>(l).begin() << " to " << (void*)std::get<0>(l).end() << std::endl;
-    std::cout << "int*: " << std::get<1>(l).begin() << " to " << std::get<1>(l).end() << std::endl;
-    std::cout << "long*: " << (void*)std::get<2>(l).begin() << " to " << (void*)std::get<2>(l).end() << std::endl;
+template<class A, class B, class C>
+void test_combinations()
+{
+    for (int x = 0; x < 32; ++x)
+    for (int y = 0; y < 32; ++y)
+    for (int z = 0; z < 32; ++z)
+    {
+        Guard{xtd::make_contiguous_layout<A, B, C>(x,y,z)};
+        Guard{xtd::make_contiguous_layout<A, C, B>(x,y,z)};
+        Guard{xtd::make_contiguous_layout<B, A, C>(x,y,z)};
+        Guard{xtd::make_contiguous_layout<B, C, A>(x,y,z)};
+        Guard{xtd::make_contiguous_layout<C, A, B>(x,y,z)};
+        Guard{xtd::make_contiguous_layout<C, B, A>(x,y,z)};
+    }
 
-    ::operator delete((void*)std::get<0>(l).begin());
+}
+
+TEST_CASE( "Basic - stress", "[make_contiguous_layout]" )
+{
+    test_combinations<int, char, long>();
+    test_combinations<int, char, std::string>();
+    test_combinations<int, std::string, long>();
+    test_combinations<std::string, char, long>();
+    test_combinations<std::string, std::string, long>();
+    test_combinations<std::string, std::string, std::string>();
 }
