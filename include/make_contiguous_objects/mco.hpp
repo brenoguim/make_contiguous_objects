e490aabf6b8d1976b331c4dece83d4fa384a3613
Breno Rodrigues Guimaraes <brenorg@gmail.com>
2022-04-29 23:45:49 -0300

    Move functions out of Init

--- include/make_contiguous_objects/mco.hpp
+++ include/make_contiguous_objects/mco.hpp
@@ -18,37 +18,40 @@ constexpr std::uintptr_t findDistanceOfNextAlignedPosition(std::uintptr_t pos, s
 }
 
 template<class T>
-struct init
+struct Init
 {
-    init(std::size_t count) : m_count(count) {}
+    Init(std::size_t count) : m_count(count) {}
+    std::size_t numBytes() const { return m_count*sizeof(T); }
     std::size_t m_count;
-
-    void addRequiredBytes(std::size_t& pos)
-    {
-        pos += findDistanceOfNextAlignedPosition(pos, alignof(T)) + m_count*sizeof(T);
-    }
-
-    void setRange(Rng<T>& rng, std::byte*& mem)
-    {
-        mem += findDistanceOfNextAlignedPosition(reinterpret_cast<std::uintptr_t>(mem), alignof(T));
-        rng.m_begin = reinterpret_cast<T*>(mem);
-        mem += m_count*sizeof(T);
-        rng.m_end = reinterpret_cast<T*>(mem);
-    }
 };
 
+template<class T>
+void addRequiredBytes(Init<T>& init, std::size_t& pos)
+{
+    pos += findDistanceOfNextAlignedPosition(pos, alignof(T)) + init.numBytes();
+}
+
+template<class T>
+void setRange(Init<T>& init, Rng<T>& rng, std::byte*& mem)
+{
+    mem += findDistanceOfNextAlignedPosition(reinterpret_cast<std::uintptr_t>(mem), alignof(T));
+    rng.m_begin = reinterpret_cast<T*>(mem);
+    mem += init.numBytes();
+    rng.m_end = reinterpret_cast<T*>(mem);
+}
+
 template<class... Args>
-auto make_contiguous_layout(init<Args>... args) -> std::tuple<Rng<Args>...>
+auto make_contiguous_layout(Init<Args>... args) -> std::tuple<Rng<Args>...>
 {
     std::size_t numBytes = 0;
-    ((args.addRequiredBytes(numBytes), ...));
+    ((addRequiredBytes(args, numBytes), ...));
 
     auto mem = (std::byte*) ::operator new(numBytes);
 
     std::tuple<Rng<Args>...> r;
 
     std::apply([&] (auto&... targs) {
-        (args.setRange(targs, mem),...);
+        (setRange(args, targs, mem),...);
     }, r);
 
     return r;
